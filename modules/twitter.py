c81eb50d6798207031179bd900e11e7b66ca6813
Mark Wong <markwkm@gmail.com>
2010-09-12 07:15:13 -0700

    twitter module
    Read the current status of a user.  Example: .twitter someone

---
+++ modules/twitter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from httplib import HTTPConnection, HTTPSConnection
+from urllib import quote
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+def twitter(phenny, input):
+    screen_name = input.group(2)
+
+    conn = HTTPConnection('api.twitter.com')
+    conn.request('GET', '/1/users/show.json?screen_name=%s' %
+                        quote(screen_name))
+    resp = conn.getresponse()
+    conn.close()
+    request = json.loads(resp.read())
+    phenny.say('@%s: %s [%s]' % (screen_name, request['status']['text'],
+                                 request['status']['created_at']))
+
+twitter.commands = ['twitter']
+twitter.example = '.twitter <name>'
+
+if __name__ == '__main__':
+    print __doc__.strip()
